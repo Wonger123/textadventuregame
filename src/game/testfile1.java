af34ccc2b6d25893be372d94eae2201d5cf4925b
Wonger123 <andrewwong817@gmail.com>
2022-03-21 16:15:01 -0400

    First commit.

---
+++ src/game/testfile1.java
@@ -0,0 +1,10 @@
+package game;
+
+public class testfile1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a game");
+		System.out.println("Made by a team of people");
+	}
+}
