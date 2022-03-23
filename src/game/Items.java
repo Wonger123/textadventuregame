cf67d4fe56503d93dd093b1a98ecaf216281e513
Wonger123 <andrewwong817@gmail.com>
2022-03-23 10:34:12 -0400

    Created Item class.

--- src/game/Items.java
+++ src/game/Items.java
@@ -2,30 +2,34 @@ package game;
 
 import java.util.HashMap;
 
-public class Items {
-
-	public static void main(String[] args) {
-		
-		
-	}
-}
-
 class Item {
-	 String name; //unique key
-	 String description; //describe the item when you look at it
+	 String itemName; //unique key
+	 String itemDescription; //describe the item when you look at it
 	 //the location is in the Room object OR here.
-	 boolean isCarryable = true;
-	 boolean isActivated = false; //eg flashlight off or on
-	 String descrActive = ""; //description for when it's active
-	 //maybe also damage points, weight, cost in gold ...
-	 String readText = ""; //if there is writing on it
+	 boolean itemIsCarryable = true;
+	 boolean itemIsActivated = false;
 	 
 	 //constructors
+	 
+	 Item(String name, String description, boolean isCarryable, boolean isActivated) {
+		 itemName = name;
+		 itemDescription = description;
+		 itemIsCarryable = isCarryable;
+		 itemIsActivated = isActivated;
+	 }
 	 
 	 //getters
 	 
 	 //create items
-	 static void setupItems(HashMap<String,Item> itemList,
-		HashMap<String, Item> roomList) {
+	 static void setupItems(HashMap<String, Item> itemList, HashMap<String, String> roomList) {
+		 itemList.put("Shovel", new Item("Shovel", "A trusty shovel, good for digging things up", true, false));
+		 itemList.put("Pickaxe", new Item("Pickaxe", "placeholder description", true, false));
+		 itemList.put("Miner's Helmet", new Item("Miner's Helmet", "placeholder description", true, false));
+		 itemList.put("Scuba Gear", new Item("Scuba Gear", "placeholder description", true, false));
+		 itemList.put("Rocket Part 1", new Item("Part 1", "placeholder description", true, false));
+		 itemList.put("Rocket Part 2", new Item("Part 2", "placeholder description", true, false));
+		 itemList.put("Rocket Part 3", new Item("Part 3", "placeholder description", true, false));
+		 itemList.put("Rocket Part 4", new Item("Part 4", "placeholder description", true, false));
+		 itemList.put("Gold", new Item("Gold", "placeholder description", true, false));
 	 }
 }
