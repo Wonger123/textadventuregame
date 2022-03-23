a5621386394e76bec01aaa21379ed9a496a74c46
GrootBoi <gtgpro43@gmail.com>
2022-03-23 10:46:11 -0400

    Added more rooms

--- src/game/Rooms.java
+++ src/game/Rooms.java
@@ -1,12 +1,5 @@
 package game;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Rooms {
-
-	public static void main(String[] args) {}
-}
+import java.util.*;
 
 class Room {
 
@@ -14,6 +7,7 @@ class Room {
 	String displayName;
 	String description;
 	boolean isDark;
+	boolean isVisited;
 	String N, E, S, W, U, D;
 	ArrayList items = new ArrayList();
 
@@ -32,9 +26,9 @@ class Room {
 		this.D = D;
 	}
 
-	void characteristics(boolean isDark) {
+	/*void characteristics(boolean isDark) {
 		this.isDark = isDark;
-	}
+	}*/
 
 	String getExit(char dir) {
 		switch (dir) {
@@ -51,20 +45,52 @@ class Room {
 	static void setupRooms(HashMap<String, Room> roomList) {
 		Room r = new Room("Forest",
 				"You awake in a forest, the last thing that you remember was flying towards mars when you crashed into a rock and went off course. Now, lying in pieces near you, your ship is unusable. You notice some parts are missing as well. You think you should probably go find them instead of laying here until you die. \n"
-				+ "Placeholder North"
+				+ "Heading north you come across a river heading West, as well as a mine through a clearing to the east.\n"
 				+ "Placeholder East"
 				+ "Placeholder South"
 				+ "Placeholder West"
 				+ "Placeholder Up");
-		r.setExits("forest2", "desert","mountains","forestclearing", "trees", "");
-		r.characteristics(false);
+		r.setExits("forest2", "desert","mountains", "lake", "trees", "");
 		roomList.put("forest1", r);
 		
-		r = new Room("Up in the Trees",
+		r = new Room("Structure in Trees",
 				"Placeholder Description\n"
 				+ "Placeholder Down");
 		r.setExits("", "", "", "", "", "forest1");
-		r.characteristics(false);
 		roomList.put("trees", r);
+
+		r = new Room("Desert",
+				"Placeholder Description\n"
+				+ "Placeholder West");
+		r.setExits("", "", "", "Desert", "", "");
+		roomList.put("desert", r);
+
+		r = new Room("Forest",
+				"Placeholder Description\n"
+				+ "Placeholder East"
+				+ "Placeholder South"
+				+ "Placeholder West");
+		r.setExits("", "mine", "forest1", "beach", "", "");
+		roomList.put("forest2", r);
+
+		r = new Room("Mines",
+				"Placeholder Description\n"
+				+ "Placeholder East"
+				+ "Placeholder West");
+		r.setExits("", "goldmine", "", "forest2", "", "");
+		roomList.put("mine", r);
+
+		r = new Room("Gold Mine",
+				"Placeholder Description\n"
+				+ "Placeholder West");
+		r.setExits("", "", "", "mine", "", "");
+		roomList.put("goldmine", r);
+
+		r = new Room("Beach",
+				"Placeholder Description\n"
+				+ "Placeholder East"
+				+ "Placeholder South");
+		r.setExits("", "forest2", "lake", "", "", "");
+		roomList.put("beach", r);
 	}
 }
