dc9fe097eaa0bdca8e9bc13249d85274bdd4f280
GrootBoi <gtgpro43@gmail.com>
2022-03-28 10:15:29 -0400

    Cleaned Up Code

--- src/game/Items.java
+++ src/game/Items.java
@@ -18,15 +18,32 @@ class Items {
 	 }
 	 
 	 //create items
-	 static void setupItems(HashMap<String, Items> itemList, HashMap<String, String> roomList) {
-		 itemList.put("Shovel", new Items("Shovel", "A trusty shovel, good for digging things up.", true, false));
-		 itemList.put("Pickaxe", new Items("Pickaxe", "A sturdy pickaxe, good for breaking rock.", true, false));
-		 itemList.put("Miner's Helmet", new Items("Miner's Helmet", "A hard hat with a light on it.", true, false));
-		 itemList.put("Scuba Gear", new Items("Scuba Gear", "A wetsuit, and an oxygen tank for diving underwater.", true, false));
-		 itemList.put("Left Wing", new Items("Rocket Left Wing", "The left wing of the rocket.", true, false));
-		 itemList.put("Nose", new Items("Rocket Nose", "The nose of the rocket.", true, false));
-		 itemList.put("Engine", new Items("Rocket Engine", "The engine of the rocket.", true, false));
-		 itemList.put("Right Wing", new Items("Rocket Right Wing", "The right wing of the rocket.", true, false));
-		 itemList.put("Gold", new Items("Gold", "Some gold, its shiny.", true, false));
+	 static void setupItems(HashMap<String, Items> itemList, HashMap<String, Room> roomList) {
+		 Items z = new Items ("Shovel", "A trusty shovel, good for digging things up.", true, false);
+		 itemList.put("shovel", z);
+
+		 z = new Items("Pickaxe", "A sturdy pickaxe, good for breaking rock.", true, false);
+		 itemList.put("pickaxe", z);
+
+		 z = new Items("Miner's Helmet", "A hard hat with a light on it.", true, false);
+		 itemList.put("helmet", z);
+
+		 z = new Items("Scuba Gear", "A wetsuit, and an oxygen tank for diving underwater.", true, false);
+		 itemList.put("scuba", z);
+
+		 z = new Items("Rocket Left Wing", "The left wing of the rocket.", true, false);
+		 itemList.put("leftwing", z);
+
+		 z = new Items("Rocket Nose", "The nose of the rocket.", true, false);
+		 itemList.put("nose", z);
+
+		 z = new Items("Rocket Engine", "The engine of the rocket.", true, false);
+		 itemList.put("engine", z);
+
+		 z = new Items("Rocket Right Wing", "The right wing of the rocket.", true, false);
+		 itemList.put("rightwing", z);
+
+		 z = new Items("Gold", "Some gold, its shiny.", true, false);
+		 itemList.put("gold", z);
 	 }
 }
