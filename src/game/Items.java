2ff114d3b72feffae5edf276a17cc800c296d15d
Wonger123 <andrewwong817@gmail.com>
2022-03-28 10:45:29 -0400

    Cleaned up code.

--- src/game/Items.java
+++ src/game/Items.java
@@ -2,7 +2,7 @@ package game;
 
 import java.util.HashMap;
 
-class Item {
+class Items {
 	
 	 String itemName; //unique key
 	 String itemDescription; //describe the item when you look at it
@@ -10,7 +10,7 @@ class Item {
 	 boolean itemIsActivated = false;
 	 
 	 //constructor
-	 Item(String name, String description, boolean isCarryable, boolean isActivated) {
+	 Items(String name, String description, boolean isCarryable, boolean isActivated) {
 		 itemName = name;
 		 itemDescription = description;
 		 itemIsCarryable = isCarryable;
@@ -18,15 +18,32 @@ class Item {
 	 }
 	 
 	 //create items
-	 static void setupItems(HashMap<String, Item> itemList, HashMap<String, String> roomList) {
-		 itemList.put("Shovel", new Item("Shovel", "A trusty shovel, good for digging things up.", true, false));
-		 itemList.put("Pickaxe", new Item("Pickaxe", "A sturdy pickaxe, good for breaking rock.", true, false));
-		 itemList.put("Miner's Helmet", new Item("Miner's Helmet", "A hard hat with a light on it.", true, false));
-		 itemList.put("Scuba Gear", new Item("Scuba Gear", "A wetsuit, and an oxygen tank for diving underwater.", true, false));
-		 itemList.put("Left Wing", new Item("Rocket Left Wing", "The left wing of the rocket.", true, false));
-		 itemList.put("Nose", new Item("Rocket Nose", "The nose of the rocket.", true, false));
-		 itemList.put("Engine", new Item("Rocket Engine", "The engine of the rocket.", true, false));
-		 itemList.put("Right Wing", new Item("Rocket Right Wing", "The right wing of the rocket.", true, false));
-		 itemList.put("Gold", new Item("Gold", "Some gold, its shiny.", true, false));
+	 static void setupItems(HashMap<String, Items> itemList, HashMap<String, Room> roomList) {
+		 Items z = new Items ("Shovel", "A trusty shovel, good for digging things up.", true, false);
+		 itemList.put("shovel", z);
+
+		 z = new Items("Pickaxe", "A sturdy pickaxe, good for breaking rock.", true, false);
+		 itemList.put("pickaxe", z);
+
+		 z = new Items("Miner's Helmet", "A hard hat with a light on it.", true, false);
+		 itemList.put("helmet", z);
+
+		 z = new Items("Scuba Gear", "A wetsuit, and an oxygen tank for diving underwater.", true, false);
+		 itemList.put("scuba", z);
+
+		 z = new Items("Rocket Left Wing", "The left wing of the rocket.", true, false);
+		 itemList.put("leftwing", z);
+
+		 z = new Items("Rocket Nose", "The nose of the rocket.", true, false);
+		 itemList.put("nose", z);
+
+		 z = new Items("Rocket Engine", "The engine of the rocket.", true, false);
+		 itemList.put("engine", z);
+
+		 z = new Items("Rocket Right Wing", "The right wing of the rocket.", true, false);
+		 itemList.put("rightwing", z);
+
+		 z = new Items("Gold", "Some gold, its shiny.", true, false);
+		 itemList.put("gold", z);
 	 }
 }
